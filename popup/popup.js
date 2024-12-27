5c784b01cd0a9861096660030de14c6c4f334ae1
Lucas <blukiluc67@gmail.com>
2024-12-27 01:19:12 +0100

    Add countdown and error message in popup
    When the search button is clicked, a 10-second countdown begins. If the window remains open after this time, it means no photo has been selected, and an error message is displayed. The search button reappears when the popup is closed and reopened.

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,27 @@
-document.getElementById('searchButton').addEventListener('click', function () {
+const button = document.getElementById('searchButton');
+const startMenu = document.querySelector(".start");
+const countdownTimer = document.getElementById("countdown-timer");
+const countdownMenu = document.querySelector(".countdown");
+
+function countdown(start) {
+    let i = start-1;
+    let countdown = setInterval(function() {
+        console.log(i);
+        countdownTimer.innerText = i;
+        i--;
+        if (i < 0) {
+            clearInterval(countdown);
+            console.log("Compte à rebours terminé!");
+            countdownTimer.innerText = "Error: Image not found."
+            countdownTimer.style.color = "#f00"
+        }
+    }, 1000);
+}
+
+button.addEventListener('click', function () {
+    startMenu.classList.add("hidden");
+    countdownMenu.classList.remove("hidden");
+    countdown(3);
     chrome.runtime.sendMessage({ action: "callSearchFunction" }, (response) => {
         console.log("Response from background script:", response);
     });
